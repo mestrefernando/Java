19b7cf70316164b94fc2d8fd32c59d905333ccc2
Fernando Mestre <67239359+mestrefernando@users.noreply.github.com>
2021-01-02 19:54:38 +0000

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -1,7 +1,7 @@
 public class Hello {
 
     public static void main(String[] args) {
-        System.out.println("Hello, Tim");
+        System.out.println("Hello world");
 
         int myFirstNumber = (10 + 5) + (2 * 10);
         int mySecondNumber = 12;
